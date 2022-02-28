860073f75a75e1a92ff24a90c32c58e6e253c310
박규태 <qtae@kakao.com>
2022-02-28 23:05:09 +0900

    commit

---
+++ src/main/java/com/toadyism/businfo/BusinfoApplication.java
@@ -0,0 +1,14 @@
+package com.toadyism.businfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinfoApplication.class, args);
+		System.out.print("obj");
+	}
+
+}
---
+++ src/test/java/com/toadyism/businfo/BusinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toadyism.businfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
